3ebf4b60263eb823a06f2052f238f0c3cc98d994
Sergey2311 <sergey556577@gmail.com>
2019-10-10 22:44:33 +0300

    Готовый вариант

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 console.log('KFC');
 
-
 const cheeseburger = {
     id: '2382',
     name: 'Чизбургер Де Люкс',
-    avatarUrl: 'https://s82079.cdn.ngenix.net/MXahUbKht6Q6D2wGRYrAz7nu.png?dw=250',
+    photoUrl: 'https://s82079.cdn.ngenix.net/MXahUbKht6Q6D2wGRYrAz7nu.png?dw=400',
+    structure: 'Пряный горчичный соус, кетчуп, сочное филе в оригинальной панировке, лук, сыр Чеддер, огурцы на пшеничной булочке с кукурузной посыпкой, свежий салат и ломтики помидора.',
+    options: '226 Г   193 Ккал   Б:12.6г Ж:6.6г У:20.7г',
 };
 
 const hotdog = {
     id: '2382',
     name: 'Хот-дог куриный',
-    avatarUrl: 'https://s82079.cdn.ngenix.net/Wyvkk6X2vsQ7jGr8c84MzfK1.png?dw=250',
+    photoUrl: 'https://s82079.cdn.ngenix.net/Wyvkk6X2vsQ7jGr8c84MzfK1.png?dw=400',
+    structure: 'Хот-дог с куриной колбаской с травами, сладким горчичным соусом, кетчупом, маринованными огурчиками, луком и халапеньо.',
+    options: '124 Г   226 Ккал   Б:9.6г Ж:10.1г У:24.3г',
 };
 
-const avatarE1 = document.getElementById('avatar1'); 
-const avatarE2 = document.getElementById('avatar2');
-avatarE1.src = cheeseburger.avatarUrl;
-avatarE2.src = hotdog.avatarUrl;
-
-
-const authorNameE1 = document.getElementById('author-name1');
-authorNameE1.textContent = cheeseburger.name;
-
-const authorNameE2 = document.getElementById('author-name2');
-authorNameE2.textContent = hotdog.name;
+const photoE1 = document.getElementById('photo1'); 
+photoE1.src = cheeseburger.photoUrl;
+const photoE2 = document.getElementById('photo2');
+photoE2.src = hotdog.photoUrl;
+const foodNameE1 = document.getElementById('food-name1');
+foodNameE1.textContent = cheeseburger.name;
+const foodNameE2 = document.getElementById('food-name2');
+foodNameE2.textContent = hotdog.name;
+const foodStructureE1 = document.getElementById('food-structure1');
+foodStructureE1.textContent = cheeseburger.structure;
+const foodStructureE2 = document.getElementById('food-structure2');
+foodStructureE2.textContent = hotdog.structure;
+const foodOptionsE1 = document.getElementById('food-options1');
+foodOptionsE1.textContent = cheeseburger.options;
+const foodOptionsE2 = document.getElementById('food-options2');
+foodOptionsE2.textContent = hotdog.options;
